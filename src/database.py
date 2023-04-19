7b11adf683dd3ad304eb92e91875d76edbbc0c03
Giwrgos Kavalieros <kava2898@gmail.com>
2023-04-19 17:15:46 +0300

    fixed no sql connect

--- src/database.py
+++ src/database.py
@@ -2,14 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import time
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 #SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/myfastdb"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+def get_engine():
+    engine = sqlalchemy.create_engine(db_url)
+    return engine
+
+while True:
+    try:
+        engine = get_engine()
+        result = engine.execute('SELECT 1')
+        break
+    except sqlalchemy.exc.OperationalError:
+        print('MySQL server not available, retrying in 5 seconds...')
+        time.sleep(5)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
 
